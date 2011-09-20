d2ea099b4efc0a0b50d3e98d1517ce2d8603f075
mroach <mroach@mroach.com>
2011-09-20 16:34:05 +0200

    Removed some unused using statements

--- NContrib/Extensions/ArrayExtensions.cs
+++ NContrib/Extensions/ArrayExtensions.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Linq;
 
 namespace NContrib.Extensions {
 
     public static class ArrayExtensions {
 
         public static string Describe<T>(this T[] source) {
+
             if (source == null)
                 return "(null)";
 
