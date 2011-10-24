694d09e6c9e45b388f9bc3afd7b8295cfd2eae9c
mroach <mroach@mroach.com>
2011-10-24 16:46:05 +0200

    store field names as they come from the DB. implemeted a Get() method to fetch them

--- NContrib4/DynamicDataRecord.cs
+++ NContrib4/DynamicDataRecord.cs
@@ -1,22 +1,15 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Dynamic;
+using System.Globalization;
 using System.Linq;
+using NContrib.Extensions;
 
 namespace NContrib4 {
 
     public class DynamicDataRecord : DynamicObject {
 
         private readonly Dictionary<string, object> _fields;
-
-        /// <summary>Turn the field names into standard format property names.</summary>
-        /// <param name="s">field name</param>
-        /// <example>field_name => FieldName</example>
-        /// <remarks>Field names ending in _xml and that begin with a &gt; are converted to <see cref="DynamicXElement"/></remarks>
-        /// <returns></returns>
-        private static string ToPropertyName(string s) {
-            return System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(s).Replace("_", "");
-        }
 
         public DynamicDataRecord(IDataRecord dr) {
             _fields = Enumerable.Range(0, dr.FieldCount)
@@ -29,12 +22,16 @@ namespace NContrib4 {
         }
 
         public override bool TryGetMember(GetMemberBinder binder, out object result) {
-            return _fields.TryGetValue(binder.Name, out result);
+            return _fields.TryGetValue(binder.Name.ToSnakeCase().ToLower(), out result);
         }
 
         // properties are not updatable
         public override bool TrySetMember(SetMemberBinder binder, object value) {
             return false;
+        }
+
+        public object Get(string fieldName) {
+            return _fields[fieldName];
         }
 
         public dynamic ToExpandoObject() {
@@ -43,9 +40,13 @@ namespace NContrib4 {
             var d = x as IDictionary<string, object>;
 
             foreach (var p in _fields)
-                d[p.Key] = p.Value;
+                d[ToPropertyName(p.Key)] = p.Value;
 
             return x;
+        }
+
+        protected static string ToPropertyName(string name) {
+            return CultureInfo.InvariantCulture.TextInfo.ToTitleCase(name).Replace("_", "");
         }
 
         protected static KeyValuePair<string, object> GetRecord(int i, IDataRecord dr) {
@@ -57,11 +58,10 @@ namespace NContrib4 {
 
             if (name.EndsWith(autoXmlSuffix) && value.ToString().StartsWith("<")) {
                 name = name.Substring(0, name.Length - autoXmlSuffix.Length);
-                name = ToPropertyName(name);
                 return new KeyValuePair<string, object>(name, new DynamicXElement(value.ToString()));
             }
 
-            return new KeyValuePair<string, object>(ToPropertyName(name), value);
+            return new KeyValuePair<string, object>(name, value);
         }
     }
 }
