959d1ef7b146b353e2dc70e74f4492e106195fdf
mroach <mroach@mroach.com>
2012-02-07 16:36:29 +0100

    Copied a lot of XML extensions from GNV

--- NContrib/Extensions/XmlExtensions.cs
+++ NContrib/Extensions/XmlExtensions.cs
@@ -1,10 +1,89 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Xml;
+using System.Xml.Xsl;
 
 namespace NContrib.Extensions {
 
     public static class XmlExtensions {
+
+        /// <summary>
+        /// Easily adds an attribute with a value to a node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="attributeName"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static XmlAttribute AddAttribute(this XmlNode node, string attributeName, object value) {
+            var a = node.GetRootDocument().CreateAttribute(attributeName);
+            a.Value = value == null ? string.Empty : value.ToString();
+            node.Attributes.Append(a);
+            return a;
+        }
+
+        /// <summary>
+        /// Adds a child to any XmlNode object
+        /// </summary>
+        /// <param name="node">XmlNode to add to</param>
+        /// <param name="xpath">Name of the element to create</param>
+        /// <param name="value">Value to add to the element</param>
+        /// <returns>Newly create XmlNode with the added value</returns>
+        public static XmlNode AddChild(this XmlNode node, string xpath, object value = null) {
+
+            var e = node.GetRootDocument().CreateElement(xpath);
+
+            if (value != null)
+                e.InnerText = value.ToString();
+
+            node.AppendChild(e);
+
+            return e;
+        }
+
+        /// <summary>
+        /// Adds a new node called and automatically adds an ID attribute with IDs starting at 1
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="xpath"></param>
+        /// <returns></returns>
+        public static XmlNode AddChildWithAutoIdNode(this XmlNode node, string xpath) {
+            var id = 1;
+
+            if (node.ChildExists(xpath))
+                id = node.SelectNodes(xpath).Count + 1;
+
+            return AddChildWithIdNode(node, xpath, id);
+        }
+
+        /// <summary>
+        /// Creates a child node and adds a node called id with the specified id
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="xpath"></param>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static XmlNode AddChildWithIdNode(this XmlNode node, string xpath, int id) {
+            var newNode = node.AddChild(xpath);
+            newNode.AddChild("id", id);
+
+            return newNode;
+        }
+
+        /// <summary>
+        /// Tells whether or not a child element with a certain name exits as
+        /// a direct descendant of this XmlNode
+        /// </summary>
+        /// <param name="node">XmlNode to search</param>
+        /// <param name="xpath">Element name to look for</param>
+        /// <returns></returns>
+        public static bool ChildExists(this XmlNode node, string xpath) {
+            if (string.IsNullOrEmpty(xpath))
+                return false;
+
+            return node.SelectSingleNode(xpath) != null;
+        }
 
         public static string GetAttributeValue(this XmlNode node, string attributeName) {
             return node.GetAttributeValue(attributeName, (string)null);
@@ -57,6 +136,15 @@ namespace NContrib.Extensions {
             return value.IsBlank() ? fallback : value.ConvertTo<T>(cultureInfo);
         }
 
+        /// <summary>
+        /// Returns the highest level XmlDocument object for this node
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
+        public static XmlDocument GetRootDocument(this XmlNode node) {
+            return node.OwnerDocument ?? (XmlDocument)node;
+        }
+
         /// <summary>Indicates if any nodes match the given xpath</summary>
         /// <param name="node"></param>
         /// <param name="xpath"></param>
@@ -68,6 +156,63 @@ namespace NContrib.Extensions {
 
             var nodes = node.SelectNodes(xpath, namespaceManager);
             return nodes != null && nodes.Count > 0;
+        }
+
+        /// <summary>
+        /// Removes this single node
+        /// </summary>
+        /// <param name="node"></param>
+        public static void Remove(this XmlNode node) {
+            node.ParentNode.RemoveChild(node);
+        }
+
+        /// <summary>
+        /// Removes all nodes in this XmlNodeList
+        /// </summary>
+        /// <param name="nodes"></param>
+        public static void Remove(this XmlNodeList nodes) {
+            nodes.Cast<XmlNode>().ToList().ForEach(n => n.Remove());
+        }
+
+        /// <summary>
+        /// Removes the node/nodes specified by the xpath
+        /// Will not throw an exception if no node/nodes are found
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="xpath"></param>
+        public static void Remove(this XmlNode node, string xpath) {
+            node.SelectNodes(xpath).Remove();
+        }
+
+        /// <summary>
+        /// Sets a node's InnerText property to the data parameter encoded with Base64
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
+        public static void SetInnerBase64(this XmlNode n, byte[] bytes) {
+            n.InnerText = Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// Transforms an XmlNode with the specified XSL string.
+        /// </summary>
+        /// <param name="node"></param>
+        /// <param name="xsl"></param>
+        /// <param name="formatting"></param>
+        /// <returns></returns>
+        public static string TransformWithXsl(this XmlNode node, string xsl, Formatting formatting = Formatting.Indented) {
+
+            using (var sw = new StringWriter())
+            using (var xw = new XmlTextWriter(sw) {Formatting = formatting})
+            using (var sr = new StringReader(xsl))
+            using (var xtr = new XmlTextReader(sr)) {
+                var transformer = new XslCompiledTransform();
+                transformer.Load(xtr);
+                transformer.Transform(node, xw);
+
+                return sw.ToString();
+            }
         }
 
         /// <summary>
