6e014d79aeeacde347f568fa25555fa516925e63
mroach <mroach@mroach.com>
2012-04-06 12:05:03 +0200

    Added some socket extensions and utilities

---
+++ NContrib/Extensions/SocketExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace NContrib.Extensions {
+
+    public static class SocketExtensions {
+
+        /// <summary>
+        /// Attempts connecting to the given host and port with a timeout. If the timeout limit is reached,
+        /// the connection is dropped.
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <param name="timeout"></param>
+        /// <returns></returns>
+        public static bool CanConnect(this Socket socket, string host, int port, TimeSpan timeout) {
+
+            var result = socket.BeginConnect(host, port, null, null);
+            result.AsyncWaitHandle.WaitOne(timeout, true);
+
+            var connected = socket.Connected;
+
+            if (connected)
+                socket.BeginDisconnect(true, ar => {}, null);
+
+            return connected;
+        }
+    }
+}
---
+++ NContrib/Utilities/Network.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+using NContrib.Extensions;
+
+namespace NContrib.Utilities {
+
+    public static class Network {
+
+        public static bool CanConnect(string host, int port, int timeoutMilliseconds, ProtocolType protocolType = ProtocolType.Tcp) {
+            return CanConnect(host, port, TimeSpan.FromMilliseconds(timeoutMilliseconds), protocolType);
+        }
+
+        public static bool CanConnect(string host, int port, TimeSpan timeout, ProtocolType protocolType = ProtocolType.Tcp) {
+            using (var sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, protocolType)) {
+                return sock.CanConnect(host, port, timeout);
+            }
+        }
+    }
+}
