c66f75dd4b1ca522e37868e42b49949301aa24dc
mroach <mroach@mroach.com>
2012-06-05 12:22:30 +0200

    Added Fisher-Yates shuffle extension to Array

--- NContrib/Extensions/ArrayExtensions.cs
+++ NContrib/Extensions/ArrayExtensions.cs
@@ -1,8 +1,11 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace NContrib.Extensions {
 
     public static class ArrayExtensions {
+
+        private static readonly Random Randomizer = new Random();
 
         public static string Describe<T>(this T[] source) {
 
@@ -15,5 +18,21 @@ namespace NContrib.Extensions {
             return "[" + source.Select(x => x.ToString()).Join(", ") + "]";
         }
 
+        /// <summary>
+        /// Uses Fisher-Yates shuffle on the array to shuffle the order
+        /// </summary>
+        /// <seealso cref="http://www.dotnetperls.com/fisher-yates-shuffle"/>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        public static void Shuffle<T>(this T[] source) {
+
+            for (var i = source.Length; i > 1; i--) {
+
+                var j = Randomizer.Next(i);
+                var tmp = source[j];
+                source[j] = source[i - 1];
+                source[i - 1] = tmp;
+            }
+        }
     }
 }
