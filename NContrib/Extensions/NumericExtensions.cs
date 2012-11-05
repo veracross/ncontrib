a081b5c63f84bb1b40ec3ba7463564508bb19c58
mroach <mroach@mroach.com>
2012-11-05 12:27:59 +0100

    Added extension to convert a number to an ordinal using English rules

---
+++ NContrib/Extensions/NumericExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace NContrib.Extensions
+{
+    public static class NumericExtensions
+    {
+        public static string ToOrdinal(this int number)
+        {
+            if (number < 0)
+                return number.ToString(CultureInfo.InvariantCulture);
+
+            var rem = number % 100;
+
+            if (rem >= 11 && rem <= 13)
+                return number + "th";
+
+            switch (number % 10)
+            {
+                case 1:
+                    return number + "st";
+                case 2:
+                    return number + "nd";
+                case 3:
+                    return number + "rd";
+                default:
+                    return number + "th";
+            }
+        }
+    }
+}
