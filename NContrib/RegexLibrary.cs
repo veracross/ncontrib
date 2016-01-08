09e7eb58231236d2d47739a68f667db6d3ca4337
Jeremy Hamel <jhamel@JHAMEL-VM2-WIN7.breuer.com>
2016-01-08 16:37:10 -0500

    handle a wider variety of TLD lengths

--- NContrib/RegexLibrary.cs
+++ NContrib/RegexLibrary.cs
@@ -43,7 +43,7 @@
         /// <summary>
         /// A forgiving email address regex. Let email software handle real validation.
         /// </summary>
-        public const string EmailAddress = @"\b[\w._%+-]+@[\w.-]+\.[a-z]{2,4}\b";
+        public const string EmailAddress = @"\b[\w._%+-]+@[\w.-]+\.[a-z]+\b";
 
         public const string Url = @"((https?|ftp|gopher|telnet|file|notes|ms-help):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)";
 
